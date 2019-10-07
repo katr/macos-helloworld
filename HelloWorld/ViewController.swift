ea6cf1a1157226ef7a535688250e490e306953cb
Bill <billj@cray.com>
2019-10-07 08:53:24 -0500

    Initial check-in

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import Cocoa
-
+//https://www.raywenderlich.com/731-macos-development-for-beginners-part-1
+// Next: https://www.raywenderlich.com/730-macos-development-for-beginners-part-2
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var helloLabel: NSTextField!
+    
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+            name = "World"
+        }
+        //let greeting = "Hello " + name + "!"
+        // https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
+        let greeting = "Hello \(name)!!"
+        helloLabel.stringValue = greeting
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
